81d2da17950005ec733447c86e757a199032986d
felipecometti <edkhod@hotmail.com>
2020-04-01 00:42:26 -0300

    first one, will probably be followed by some minor corrections, but probably no upgrade

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # filesync
 A really simple file synchronisation routine
+
+### ...but why?
+
+This piece of code is really not meant to an environment where things are well planned.
+
+I made it to distribute some files that were synchronised to an OneDrive folder
+from a SharePoint page. A creative work around, some may say.
+
+But well:
+* It works;
+* It saved me like 10 minutes/day.
+
+### Details
+
+To call sync when imported:
+Provide sync(name, src, dst)
+
+Being:
+name: file name, purely cosmetic
+src: source path
+dst: destination path
+
+subs() is only used inside sync()
+
+It gives a lot of text feedback, might annoy some.
---
+++ synchronise.py
@@ -0,0 +1,91 @@
+import os
+from shutil import copyfile
+
+import datetime
+timestart = datetime.datetime.now()
+
+#===================================================================#
+#	Synchronisation block for files already available on dirs	    #
+#	Doesn't work as a connector (I don't know why I made this note) #
+#===================================================================#
+
+def subs(name, src, dst):
+	# Runs shutil.copyfile
+	# Erros aren't treated. If it runs into one, the program ends
+	# The 'name' variable is largely cosmetic
+	print ("Copying: " + name)
+	try:
+		copyfile(src, dst)
+	except:
+		print("There was an error")
+		return 1
+	print("OK.")
+	
+
+def sync(name, src, dst):
+	# Compares the to files
+	# If os.path.getmtime(src) > os.path.getmtime(dst), dst gets replaced by src
+	# If os.path.getmtime(src) <= os.path.getmtime(dst), nothing happens
+	# If os.path.getmtime(dst) returns OSError, it tries to run the replacement
+	# If the replacement throws any error, the program ends
+
+	timestart = datetime.datetime.now()
+	
+	try:
+		os.path.getmtime(dst)
+	except OSError:
+		print(name + " wasn't found in the destination, or had it's access denied.")
+		print("Trying to send the file to the destination...")
+		subs(name, src, dst)
+		
+		timeend = datetime.datetime.now()
+		deltatime = timeend - timestart
+		print("Elapsed time: [" + str(deltatime) + "]")
+		
+		return 2
+		
+	try:
+		os.path.getmtime(src)
+	except OSError:
+		print(name + " wasn't found on the origin.")
+		return 3
+	
+	srcmtime = os.path.getmtime(src)
+	dstmtime = os.path.getmtime(dst)
+	
+	if(srcmtime > dstmtime):
+		subs(name, src, dst)
+	else:
+		print("Copying: " + name)
+		print(name + " is already up to date.")
+		return 4
+
+	timeend = datetime.datetime.now()
+	deltatime = timeend - timestart
+	print("Elapsed time: [" + str(deltatime) + "]")
+
+#===================================================================#
+#	End of the block                                                #
+#	--------------------                                            #
+#	Start of the call routines                                      #
+#===================================================================#
+
+name = "Test"
+src = "C:/Users/John Doe/Desktop/test/test1/updatedtest.txt"
+dst = "C:/Users/John Doe/Desktop/test/test2/mytest.txt"
+sync(name, src, dst)
+
+# Copy and paste as many as you want
+
+#===================================================================#
+#	End of the call routine                                         #
+#	--------------------                                            #
+#	THE END                                                         #
+#===================================================================#
+
+timeend = datetime.datetime.now()
+deltatime = timeend - timestart
+
+print("\nFinished.")
+print("Total time: [" + str(deltatime) + "]")
+input("\nPress enter to continue...")
